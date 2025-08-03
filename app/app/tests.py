2b52aed6327ca4c03bc5490329b8d4eba394c5c8
shashankkanchan <shashankkanchan200@gmail.com>
2025-08-03 10:04:11 +0530

    postgress sql adapter config

--- app/app/tests.py
+++ app/app/tests.py
@@ -3,12 +3,13 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
+
     def test_add_numbers(self):
         res = calc.add(5, 6)
-        
         self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
         res = calc.subtract(5, 10)
-        self.assertEqual(res, 5)   
+        self.assertEqual(res, 5)
